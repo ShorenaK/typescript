72abebbdbf0fb488f5eb0d71fce588e6bcc2c08e
ShorenaK <shorenaanzhilov@gmail.com>
2023-02-01 17:42:01 -0500

    trying new ver

--- index.ts
+++ index.ts
@@ -4,3 +4,9 @@ console.log(character)
 const inputs = document.querySelectorAll('input')
 
 console.log(inputs)
+inputs.forEach(input =>{
+    console.log(input)
+})
+
+let age = 30 
+let isBlackBelt = false
