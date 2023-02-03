a6e1e4af6f2574a286e9e98cc9086a897514014c
ShorenaK <shorenaanzhilov@gmail.com>
2023-02-03 11:38:35 -0500

    app.ts

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
 "use strict";
-const anchor = document.querySelector('a');
+// const anchor = document.querySelector('a')!
 // if(anchor){
 // console.log(anchor.href)
 // }
-// console.log(anchor.href);
+// console.log(anchor.href)
+// const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form');
+console.log(form.children);
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
-const anchor = document.querySelector('a')
+// const anchor = document.querySelector('a')!
 // if(anchor){
 // console.log(anchor.href)
 // }
-console.log(anchor.href)
+// console.log(anchor.href)
+
+// const form = document.querySelector('form')!;
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+console.log(form.children);
+
+// inputs 
+
